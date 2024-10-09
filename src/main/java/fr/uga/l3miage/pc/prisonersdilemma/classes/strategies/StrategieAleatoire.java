439acd9b3cfd3d1602a63d1cadea8e0442dc3519
l3miage-bensaisa <sami.bensaid@etu.univ-grenoble-alpes.fr>
2024-10-09 12:41:12 +0200

    Changement du type de Random utilisé

--- src/main/java/fr/uga/l3miage/pc/prisonersdilemma/classes/strategies/StrategieAleatoire.java
+++ src/main/java/fr/uga/l3miage/pc/prisonersdilemma/classes/strategies/StrategieAleatoire.java
@@ -1,15 +1,15 @@
 package fr.uga.l3miage.pc.prisonersdilemma.classes.strategies;
 
+import java.security.SecureRandom;
 import java.util.List;
-import java.util.Random;
 
 import fr.uga.l3miage.pc.prisonersdilemma.classes.PartieJouee;
 
 public class StrategieAleatoire extends Strategie{
-    private final Random rand = new Random();
+    SecureRandom random = new SecureRandom();
 
     @Override
     public boolean jouer(List<PartieJouee> historique){
-        return this.rand.nextInt(100) > 50;
+        return this.random.nextInt(100) > 50;
     }
 }
